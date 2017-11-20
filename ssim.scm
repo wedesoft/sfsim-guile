277b8f2871fddc06cc33ec00a801b266b9842a20
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-20 21:58:20 +0000

    Display empty window

--- ssim.scm
+++ ssim.scm
@@ -1,3 +1,13 @@
 #!/usr/bin/env guile
 !#
-(format #t "Hello\n")
+(use-modules (glut) (gl))
+
+
+(define (on-display)
+  (gl-clear (clear-buffer-mask color-buffer))
+  (swap-buffers))
+
+(initialize-glut (program-arguments) #:window-size '(640 . 480) #:display-mode (display-mode rgb double))
+(define main-window (make-window "ssim"))
+(set-display-callback on-display)
+(glut-main-loop)
