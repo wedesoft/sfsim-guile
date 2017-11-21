0b9089cec91a44e78dbd4707a738d2270de754e6
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-21 20:08:35 +0000

    Quaternion class

--- ssimlib/quaternion.scm
+++ ssimlib/quaternion.scm
@@ -1,6 +1,20 @@
 (define-module (ssimlib quaternion)
+  #:use-module (oop goops)
+  #:use-module (ice-9 format)
   #:use-module (ice-9 optargs)
-  #:export (make-quaternion))
+  #:export (<quaternion>
+            make-quaternion jmag-part kmag-part)
+  #:re-export (real-part imag-part))
 
 
-(define* (make-quaternion #:optional (a 0) (b 0) (c 0) (d 0)) (list a b c d))
+(define-class <quaternion> ()
+  (real-part #:init-keyword #:real-part #:getter real-part)
+  (imag-part #:init-keyword #:imag-part #:getter imag-part)
+  (jmag-part #:init-keyword #:jmag-part #:getter jmag-part)
+  (kmag-part #:init-keyword #:kmag-part #:getter kmag-part))
+
+(define* (make-quaternion #:optional (a 0) (b 0) (c 0) (d 0))
+  (make <quaternion> #:real-part a #:imag-part b #:jmag-part c #:kmag-part d))
+
+(define-method (write (self <quaternion>) port)
+  (format port "~f~@fi~@fj~@fk" (real-part self) (imag-part self) (jmag-part self) (kmag-part self)))
--- tests/test_ssim.scm
+++ tests/test_ssim.scm
@@ -1,12 +1,17 @@
 (use-modules (srfi srfi-64)
+             (srfi srfi-26)
              (ssimlib quaternion))
 
 
 (test-begin "ssim")
 (test-begin "quaternion")
 (test-equal "Create quaternion"
-  '(0 0 0 0) (make-quaternion))
+  '(0 0 0 0) (map (cut <> (make-quaternion)) (list real-part imag-part jmag-part kmag-part)))
 (test-equal "Initialise quaternion with values"
-  '(2 3 5 7) (make-quaternion 2 3 5 7))
+  '(2 3 5 7) (map (cut <> (make-quaternion 2 3 5 7)) (list real-part imag-part jmag-part kmag-part)))
+(test-equal "Display quaternion"
+  "2.0+3.0i+5.0j+7.0k" (call-with-output-string (lambda (port) (write (make-quaternion 2 3 5 7) port))))
+(test-equal "Display negative components"
+  "-2.0-3.0i-5.0j-7.0k" (call-with-output-string (lambda (port) (write (make-quaternion -2 -3 -5 -7) port))))
 (test-end "quaternion")
 (test-end "ssim")
