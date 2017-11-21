0ab18bbd78efb24eef276e168e5528e214284b1d
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-21 17:16:38 +0000

    Initialize quaternion with values

--- ssimlib/quaternion.scm
+++ ssimlib/quaternion.scm
@@ -1,5 +1,6 @@
 (define-module (ssimlib quaternion)
+  #:use-module (ice-9 optargs)
   #:export (make-quaternion))
 
 
-(define (make-quaternion) (make-list 4 0))
+(define* (make-quaternion #:optional (a 0) (b 0) (c 0) (d 0)) (list a b c d))
--- tests/test_ssim.scm
+++ tests/test_ssim.scm
@@ -6,5 +6,7 @@
 (test-begin "quaternion")
 (test-equal "Create quaternion"
   '(0 0 0 0) (make-quaternion))
+(test-equal "Initialise quaternion with values"
+  '(2 3 5 7) (make-quaternion 2 3 5 7))
 (test-end "quaternion")
 (test-end "ssim")
