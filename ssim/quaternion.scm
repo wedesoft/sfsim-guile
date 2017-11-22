3cb16bf04d95d5a207b7d08d6b2808f837460b31
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-11-22 19:20:42 +0000

    Map to complex or real if possible

--- ssim/quaternion.scm
+++ ssim/quaternion.scm
@@ -19,50 +19,41 @@
 
 (define* (make-quaternion #:optional (a 0) (b 0) (c 0) (d 0))
   (if (and (zero? c) (zero? d))
-    (if (zero? b) a (make-rectangular a b))
+    (make-rectangular a b)
     (make-quaternion2 a b c d)))
 
 (define-method (jmag-part (self <number>)) 0)
 (define-method (kmag-part (self <number>)) 0)
 
+(define (components self)
+  (map (cut <> self) (list real-part imag-part jmag-part kmag-part)))
+
 (define-method (write (self <quaternion>) port)
-  (format port "~f~@fi~@fj~@fk" (real-part self) (imag-part self) (jmag-part self) (kmag-part self)))
+  (apply format port "~f~@fi~@fj~@fk" (components self)))
 
 (define-syntax-rule (quaternion-binary-op op)
   (begin
-    (define-method (op (a <real>) (b <quaternion>)) (op (make-quaternion2 a 0 0 0) b))
-    (define-method (op (a <quaternion>) (b <real>)) (op a (make-quaternion2 b 0 0 0)))
-    (define-method (op (a <complex>) (b <quaternion>)) (op (make-quaternion2 (real-part a) (imag-part a) 0 0) b))
-    (define-method (op (a <quaternion>) (b <complex>)) (op a (make-quaternion2 (real-part b) (imag-part b) 0 0)))))
+    (define-method (op (a <real>      ) (b <quaternion>)) (op (make-quaternion2 a 0 0 0) b))
+    (define-method (op (a <quaternion>) (b <real>      )) (op a (make-quaternion2 b 0 0 0)))
+    (define-method (op (a <complex>   ) (b <quaternion>)) (op (make-quaternion2 (real-part a) (imag-part a) 0 0) b))
+    (define-method (op (a <quaternion>) (b <complex>   )) (op a (make-quaternion2 (real-part b) (imag-part b) 0 0)))))
 
 (define-method (equal? (a <quaternion>) (b <quaternion>))
-  (and (equal? (real-part a) (real-part b))
-       (equal? (imag-part a) (imag-part b))
-       (equal? (jmag-part a) (jmag-part b))
-       (equal? (kmag-part a) (kmag-part b))))
+  (and-map (cut apply equal? <>) (map list (components a) (components b))))
 
 (define-method (= (a <quaternion>) (b <quaternion>))
-  (and (= (real-part a) (real-part b))
-       (= (imag-part a) (imag-part b))
-       (= (jmag-part a) (jmag-part b))
-       (= (kmag-part a) (kmag-part b))))
+  (and-map (cut apply = <>) (map list (components a) (components b))))
 (quaternion-binary-op =)
 
 (define-method (- (a <quaternion>))
-  (make-quaternion (- (real-part a)) (- (imag-part a)) (- (jmag-part a)) (- (kmag-part a))))
+  (apply make-quaternion (map - (components a) )))
 
 (define-method (+ (a <quaternion>) (b <quaternion>))
-  (make-quaternion (+ (real-part a) (real-part b))
-                   (+ (imag-part a) (imag-part b))
-                   (+ (jmag-part a) (jmag-part b))
-                   (+ (kmag-part a) (kmag-part b))))
+  (apply make-quaternion (map + (components a) (components b))))
 (quaternion-binary-op +)
 
 (define-method (- (a <quaternion>) (b <quaternion>))
-  (make-quaternion (- (real-part a) (real-part b))
-                   (- (imag-part a) (imag-part b))
-                   (- (jmag-part a) (jmag-part b))
-                   (- (kmag-part a) (kmag-part b))))
+  (apply make-quaternion (map - (components a) (components b))))
 
 (define-method (* (a <quaternion>) (b <quaternion>))
   (make-quaternion
@@ -82,11 +73,10 @@
              (* (imag-part a) (jmag-part b)))
           (* (jmag-part a) (imag-part b)))
        (* (kmag-part a) (real-part b)))))
+(quaternion-binary-op *)
 
 (define (quaternion-rotation theta axis)
   (let* [(theta2     (/ theta 2))
          (cos-theta2 (cos theta2))
          (sin-theta2 (sin theta2))]
     (apply make-quaternion cos-theta2 (map (cut * <> sin-theta2) axis))))
-
-(quaternion-binary-op *)
--- tests/test_quaternion.scm
+++ tests/test_quaternion.scm
@@ -6,31 +6,36 @@
 (define pi (* 2 (acos 0)))
 
 (test-begin "ssim quaternion")
-(test-begin "quaternion objects")
-(test-equal "Create quaternion"
-  '(0 0 0 0) (map (cut <> (make-quaternion)) (list real-part imag-part jmag-part kmag-part)))
-(test-equal "Initialise quaternion with values"
-  '(2 3 5 7) (map (cut <> (make-quaternion 2 3 5 7)) (list real-part imag-part jmag-part kmag-part)))
-(test-equal "Display quaternion"
-  "2.0+3.0i+5.0j+7.0k" (call-with-output-string (lambda (port) (write (make-quaternion 2 3 5 7) port))))
-(test-equal "Display negative components"
-  "-2.0-3.0i-5.0j-7.0k" (call-with-output-string (lambda (port) (write (make-quaternion -2 -3 -5 -7) port))))
-(test-equal "Equal quaternions"
-  (make-quaternion 2 3 5 7) (make-quaternion 2 3 5 7))
+  (test-begin "quaternion objects")
+  (test-equal "Create quaternion"
+    '(0 0 0 0) (map (cut <> (make-quaternion)) (list real-part imag-part jmag-part kmag-part)))
+  (test-equal "Initialise quaternion with values"
+    '(2 3 5 7) (map (cut <> (make-quaternion 2 3 5 7)) (list real-part imag-part jmag-part kmag-part)))
+  (test-equal "Display quaternion"
+    "2.0+3.0i+5.0j+7.0k" (call-with-output-string (lambda (port) (write (make-quaternion 2 3 5 7) port))))
+  (test-equal "Display negative components"
+    "-2.0-3.0i-5.0j-7.0k" (call-with-output-string (lambda (port) (write (make-quaternion -2 -3 -5 -7) port))))
+  (test-equal "Map to complex if possible"
+    2.0+3.0i (make-quaternion 2.0 3.0 0.0 0.0))
+  (test-equal "Map to scalar if possible"
+    1 (make-quaternion 1 0 0 0))
 (test-end "quaternion objects")
 
 (test-begin "equality")
-(let [(i (make-quaternion 0 1 0 0))
-      (j (make-quaternion 0 0 1 0))
-      (k (make-quaternion 0 0 0 1))]
-  (for-each (lambda (b)
-    (test-assert (format #f "Compare zero with ~a" b)
-      (not (equal? (make-quaternion 0 0 0 0) b))))
-    (list 1 i j k))
-  (test-equal "Compare zero quaternion with scalar"
-    0 (make-quaternion 0 0 0 0))
-  (test-equal "Compare quaternion with complex number"
-    3+5i (make-quaternion 3 5 0 0)))
+  (test-equal "Equal quaternions"
+    (make-quaternion 2 3 5 7) (make-quaternion 2 3 5 7))
+  (let [(a (make-quaternion 0 1 1 1))
+        (b (make-quaternion 1 0 1 1))
+        (c (make-quaternion 1 1 0 1))
+        (d (make-quaternion 1 1 1 0))]
+    (for-each (lambda (q)
+      (test-assert (format #f "Compare 1+i+j+k with ~a" q)
+        (not (equal? (make-quaternion 1 1 1 1) q))))
+      (list a b c d))
+    (test-equal "Positive comparison of quaternion"
+      (make-quaternion 2 3 5 7) (make-quaternion 2 3 5 7))
+    (test-assert "Compare different component types"
+      (not (equal? (make-quaternion 3.0 5 7 11) (make-quaternion 3 5 7 11)))))
 (test-end "equality")
 
 (test-begin "comparison with =")
@@ -62,6 +67,10 @@
     (make-quaternion 5 3 5 7) (+ (make-quaternion 2 3 5 7) 3))
   (test-equal "Sum of real number and quaternion"
     (make-quaternion 5 3 5 7) (+ 3 (make-quaternion 2 3 5 7)))
+  (test-equal "Sum of quaternion and complex number"
+    (make-quaternion 5.0 8.0 5 7) (+ (make-quaternion 2 3 5 7) 3+5i))
+  (test-equal "Sum of complex number and quaternion"
+    (make-quaternion 5.0 8.0 5 7) (+ 3+5i (make-quaternion 2 3 5 7)))
   (test-equal "Difference of quaternions"
     (make-quaternion -1 -2 -2 -4) (- (make-quaternion 2 3 5 7) (make-quaternion 3 5 7 11)))
   (let [(i2 (make-quaternion 0 2 0 0))
@@ -70,7 +79,7 @@
     (for-each (lambda (a results)
       (for-each (lambda (b expected)
           (test-assert (format #f "Multiplying ~a with ~a is expected to be ~a" a b expected)
-            (zero? (- (* a b) expected)))
+            (= (* a b) expected))
           (format #t "# ~a~&" expected))
         (list 2 i j k) results))
       (list 2 i j k)
