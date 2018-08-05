1666d1bc7bf9a71d0f99fd1ac5cc3e1f90c8e39a
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-08-05 21:58:18 +0100

    Refactor GJK prototype

--- etc/gjk.rb
+++ etc/gjk.rb
@@ -56,37 +56,12 @@ end
 def simplex_closest_point simplex
   if simplex.size == 1
     return simplex[0], simplex
-  elsif simplex.size == 2
+  else
     p = simplex[0]
-    n = simplex[1] - p
-    t = Linalg.lstsq(NArray[n.to_a].transpose, -NArray[*p])[0][0]
-    if t >= 0 and t <= 1
-      return p + n * t, simplex
-    else
-      simplex.subsets.collect do |s|
-        simplex_closest_point s
-      end.argmin { |p, s| p.norm }
-    end
-  elsif simplex.size == 3
-    p = simplex[0]
-    n1 = simplex[1] - p
-    n2 = simplex[2] - p
-    t = Linalg.lstsq(NArray[n1.to_a, n2.to_a].transpose, -NArray[*p])[0].to_a
-    if t[0] >= 0 and t[1] >= 0 and t[0] + t[1] <= 1
-      return p + n1 * t[0] + n2 * t[1], simplex
-    else
-      simplex.subsets.collect do |s|
-        simplex_closest_point s
-      end.argmin { |p, s| p.norm }
-    end
-  elsif simplex.size == 4
-    p = simplex[0]
-    n1 = simplex[1] - p
-    n2 = simplex[2] - p
-    n3 = simplex[3] - p
-    t = Linalg.lstsq(NArray[n1.to_a, n2.to_a, n3.to_a].transpose, -NArray[*p])[0].to_a
-    if t[0] >= 0 and t[1] >= 0 and t[2] >= 0 and t[0] + t[1] + t[2] <= 1
-      return p + n1 * t[0] + n2 * t[1] + n3 * t[2], simplex
+    n = simplex[1 ... simplex.size].collect { |s| (s - p).to_a }
+    t = Linalg.lstsq(NArray[*n].transpose, -NArray[*p])[0].to_a
+    if t.all? { |ti| ti >= 0 } and t.inject(:+) <= 1
+      return p + n.zip(t) { |ni, ti| ni * ti }.inject(:+), simplex
     else
       simplex.subsets.collect do |s|
         simplex_closest_point s
@@ -95,4 +70,4 @@ def simplex_closest_point simplex
   end
 end
 
-gjk [Vector[1, 1], Vector[3, 1], Vector[2, -2], Vector[1, 5]]
+gjk [Vector[1, 0], Vector[3, 1], Vector[2, -2], Vector[2, 5]]
