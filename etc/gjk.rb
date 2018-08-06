2805fc416b0976170a20805347176293d3852d97
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-08-06 21:16:18 +0100

    Bugfix GJK prototype

--- etc/gjk.rb
+++ etc/gjk.rb
@@ -58,10 +58,10 @@ def simplex_closest_point simplex
     return simplex[0], simplex
   else
     p = simplex[0]
-    n = simplex[1 ... simplex.size].collect { |s| (s - p).to_a }
-    t = Linalg.lstsq(NArray[*n].transpose, -NArray[*p])[0].to_a
+    n = simplex[1 ... simplex.size].collect { |s| s - p }
+    t = Linalg.lstsq(NArray[*n.collect(&:to_a)].transpose, -NArray[*p])[0].to_a
     if t.all? { |ti| ti >= 0 } and t.inject(:+) <= 1
-      return p + n.zip(t) { |ni, ti| ni * ti }.inject(:+), simplex
+      return p + n.zip(t).collect { |ni, ti| ni * ti }.inject(:+), simplex
     else
       simplex.subsets.collect do |s|
         simplex_closest_point s
@@ -70,4 +70,10 @@ def simplex_closest_point simplex
   end
 end
 
-gjk [Vector[1, 0], Vector[3, 1], Vector[2, -2], Vector[2, 5]]
+a = [Vector[1, 4], Vector[3, 4], Vector[3, 1], Vector[1, 1]]
+b = [Vector[-1, 3], Vector[1, 2], Vector[-1, 2]]
+d = minkowski_difference a, b
+gjk d
+
+simplex_closest_point [Vector[-1, -1], Vector[3, 2]]
+simplex = [Vector[-1, -1], Vector[3, 2]]
