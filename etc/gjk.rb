c71e28509dc530fddfab515ab4771c53d19a0ce2
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-08-07 22:06:46 +0100

    Prototype collision detection

--- etc/gjk.rb
+++ etc/gjk.rb
@@ -44,44 +44,38 @@ def average points
   points.inject(:+) * (1.0 / points.size)
 end
 
-def gjk points
+def vector arg
+  arg[0] - arg[1]
+end
+
+def gjk a, b
   w = []
-  v = average points
-  t = [1]
+  v = [average(a), average(b)]
   while true
-    wk = support_point points, -v
-    return v, w, t if support(v, -v) >= support(wk, -v) - 1e-6
+    wk = [support_point(a, -vector(v)), support_point(b, +vector(v))]
+    return v, w if support(vector(v), -vector(v)) >= support(vector(wk), -vector(v)) - 1e-6
     w += [wk]
-    v, w, t = simplex_closest_point w
+    p w
+    v, w = simplex_closest_point w
   end
 end
 
 def simplex_closest_point simplex
   if simplex.size == 1
-    return simplex[0], simplex, [1]
+    return simplex[0], simplex
   else
-    p = simplex[0]
-    n = simplex[1 ... simplex.size].collect { |s| s - p }
+    p = vector(simplex[0])
+    n = simplex[1 ... simplex.size].collect { |s| vector(s) - p }
     t = Linalg.lstsq(NArray[*n.collect(&:to_a)].transpose, -NArray[*p])[0].to_a
     if t.all? { |ti| ti >= 0 } and t.inject(:+) <= 1
-      return p + n.zip(t).collect { |ni, ti| ni * ti }.inject(:+), simplex, t
+      pa = simplex[0][0] + (1 ... simplex.size).zip(t).collect { |i, ti| (simplex[i][0] - simplex[0][0]) * ti }.inject(:+)
+      pb = simplex[0][1] + (1 ... simplex.size).zip(t).collect { |i, ti| (simplex[i][1] - simplex[0][1]) * ti }.inject(:+)
+      return [pa, pb], simplex
     else
       simplex.subsets.collect do |s|
         simplex_closest_point s
-      end.argmin { |p, s, t| p.norm }
+      end.argmin { |p, s| vector(p).norm }
     end
-  end
-end
-
-def lookup a, b, simplex
-  simplex.collect do |s|
-    result = nil
-    for x in a
-      for y in b
-        result = [x, y] if x - y == s
-      end
-    end
-    result
   end
 end
 
@@ -89,14 +83,4 @@ a = [Vector[1, 4], Vector[3, 4], Vector[3, 1], Vector[1, 1]]
 #b = [Vector[-1, 3], Vector[0.9, 2], Vector[-1, 2]]
 b = [Vector[-1, 3], Vector[-1, 2], Vector[0.9, 2]]
 d = minkowski_difference a, b
-c = gjk d
-c[0] # vector
-c[1] # simplex
-c[2] # linear-combination
-lookup(a, b, c[1])
-# linear-combine collision point
-# t und x oder t und y -> Kollisionspunkt
-# plane-fit SVD von Vektoren vom Punkt aus -> Normale
-# oder: epsilon Abstand ergibt Normale
-# [0.9, 2]
-# [1, 2]
+c = gjk a, b
