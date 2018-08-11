87424ab8fd2dbbf1499c119a4e1b44c1e6c697e4
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-08-11 23:07:02 +0100

    Simulate two colliding objects

--- sfsim/sfsim.scm
+++ sfsim/sfsim.scm
@@ -22,8 +22,8 @@
 (define time #f)
 (define main-window #f)
 
-(define state1 (make-state '(0 0.3 0) '(0 -0.2 0) (quaternion-rotation 0 '(1 0 0)) '(0.0 0.0 0.0)))
-(define state2 (make-state '(0 -0.3 0) '(0 0.2 0) (quaternion-rotation 0 '(1 0 0)) '(0.0 0.0 0.0)))
+(define state1 (make-state '(0 0.6 0) '(0 -0.1 0) (quaternion-rotation 0 '(1 0 0)) '(0.005 0.02 0.01)))
+(define state2 (make-state '(0 -0.6 0) '(0 0.1 0) (quaternion-rotation 0 '(1 0 0)) '(0.015 0.005 0.01)))
 (define g '(0 -0.5 0))
 
 (define m 1)
@@ -39,7 +39,7 @@
 (define dtmax 0.025)
 (define epsilon (* 0.5 (abs (cadr g)) (* dtmax dtmax)))
 (define ve (sqrt (* 2 (abs (cadr g)) epsilon)))
-(define max-depth 5)
+(define max-depth 10)
 
 (define g '(0 0 0))
 (define speed-scale 0.3)
@@ -102,6 +102,34 @@
                   (+ (angular-momentum state) (cross-product radius impulse-vector)))
       state)))
 
+(define (collide closest state1 state2)
+  (let* [(radius1        (- (car closest) (position state1)))
+         (radius2        (- (cdr closest) (position state2)))
+         (s1             (+ (speed state1) (cross-product (angular-velocity inertia (orientation state1) (angular-momentum state1)) radius1)))
+         (s2             (+ (speed state2) (cross-product (angular-velocity inertia (orientation state2) (angular-momentum state2)) radius2)))
+         (relative-speed (- s1 s2))
+         (normal         (normalize (- (car closest) (cdr closest))))
+         (speed-delta    (deflect relative-speed normal loss mu ve))
+         (direction      (normalize speed-delta))
+         (impulse        (/ (norm speed-delta)
+                            (+ (/ 1 m) (/ 1 m) (inner-product direction (cross-product (dot (inverse (inertia (orientation state1)))
+                                                                                       (cross-product radius1 direction)) radius1)))))
+         (impulse-vector (* impulse direction))]
+    (if (< (inner-product normal relative-speed) 0)
+     (cons (make-state (position state1)
+                  (+ (speed state1) (* (/ 1 m) impulse-vector))
+                  (quaternion-normalize (orientation state1))
+                  (+ (angular-momentum state1) (cross-product radius1 impulse-vector)))
+          (make-state (position state2)
+                  (+ (speed state2) (* (/ 1 m) (- impulse-vector)))
+                  (quaternion-normalize (orientation state2))
+                  (+ (angular-momentum state2) (cross-product radius2 (- impulse-vector))))
+    )
+     (cons state1 state2)
+     )
+    )
+)
+
 (define (depth state corner)
   (- ground (cadr (particle-position state corner))))
 
@@ -116,7 +144,7 @@
            (d       (- (norm (-(car closest) (cdr closest)))))]
       (if (>= d (* -2 epsilon))
         (if (or (<= d (- epsilon)) (>= recursion max-depth))
-          (cons state1 state2)
+          (begin (format #t "d ~a~&" d) (collide closest update1 update2))
           (let [(update (timestep state1 state2 (/ dt 2) (1+ recursion)))]
             (timestep (car update) (cdr update) (/ dt 2) (1+ recursion))))
         (cons update1 update2)))))
