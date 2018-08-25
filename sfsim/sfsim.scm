ec333bf472f55aecaecc76267b7859ce4701ff61
Jan Wedekind (Dr) <jan@wedesoft.de>
2018-08-25 22:46:59 +0100

    Prototype gear contact

--- sfsim/sfsim.scm
+++ sfsim/sfsim.scm
@@ -21,9 +21,9 @@
 (define time #f)
 (define main-window #f)
 
-(define state1 (make-state '(0 0.3 0) '(0 0 0) (quaternion-rotation (/ 3.1415926 2) '(0 1 0)) '(0.0 0.0 0.0)))
+(define state1 (make-state '(0 0.3 0) '(0 -0.2 0) (quaternion-rotation 0 '(0 1 0)) '(0.0 0.0 0.0)))
 (define state2 (make-state '(0 -0.3 0) '(0 0 0) (quaternion-rotation 0 '(0 0 1)) '(0.0 0.0 0.0)))
-(define gear  (make-spring 0.05 0.0))
+(define gear  (make-spring 0.0 0.0))
 (define lander (make-lander state1 gear))
 
 (define g '(0 -0.5 0))
@@ -31,12 +31,12 @@
 (define m1 1)
 (define m2 5.9742e+24)
 (define mg 0.05)
-(define K 10.0)
-(define D 0.1)
+(define K 20.0)
+(define D 0.5)
 (define w 0.5)
 (define h 0.1)
 (define d 0.25)
-(define gear-pos (list (/ w 2) (- h) 0))
+(define gear-pos (list 0 (- (* 1.5 h)) 0))
 (define inertia1 (inertia-body (cuboid-inertia m1 w h d)))
 (define inertia2 (inertia-body (cuboid-inertia m2 w h d)))
 
@@ -113,22 +113,31 @@
   (show-state state2 scale)
   (swap-buffers))
 
-(define* (timestep lander1 state2 gear dt #:optional (recursion 0))
-  (let [(update1 (runge-kutta lander1 dt (lander-change m1 inertia1 '(0 0 0) K D 0.1 gear-pos)))
+(define* (timestep lander1 state2 dt #:optional (recursion 0))
+  (let [(update1 (runge-kutta lander1 dt (lander-change m1 inertia1 '(0 -0.1 0) K D 0.1 gear-pos)))
         (update2 (runge-kutta state2 dt (state-change m2 inertia2 '(0 0 0) '(0 0 0))))]
     (let* [(closest  (gjk-algorithm (body1 (state update1)) (body2 update2)))
-           (distance       (norm (- (car closest) (cdr closest))))]
-      (if (and (eqv? recursion 0) (>= distance epsilon))
+           (distance (norm (- (car closest) (cdr closest))))
+           (closest2 (gjk-algorithm (list (particle-position (state update1) (+ gear-pos (list 0 (position (car (gears update1))) 0)))) (body2 update2)))
+           (distance2 (norm (- (car closest2) (cdr closest2))))]
+      (if (and (eqv? recursion 0) (>= (min distance distance2) epsilon))
         (list update1 update2)
-        (if (or (>= distance epsilon) (>= recursion max-depth))
-          (let [(c (collision (state update1) update2 m1 m2 inertia1 inertia2 closest loss mu ve))]
-            (list (apply make-lander (car c) (gears update1)) (cdr c) gear))
-          (timestep lander1 state2 gear (/ dt 2) (1+ recursion)))))))
+        (if (or (>= (min distance distance2) epsilon) (>= recursion max-depth))
+          (if (<= distance distance2)
+            (let [(c (collision (state update1) update2 m1 m2 inertia1 inertia2 closest loss mu ve))]
+              (list (apply make-lander (car c) (gears update1)) (cdr c)))
+            (let* [(g  (car (gears update1)))
+                   (v1 (cadr (linear-momentum (state update1))))
+                   (v2 (speed (car (gears update1))))
+                   (v  (+ (- (* 2 v1)) (- v2)))]
+              (list (make-lander (state update1) (make-spring (position g) v)) update2)))
+          (timestep lander1 state2 (/ dt 2) (1+ recursion)))))))
 
 (define (on-idle)
   (let [(dt (min dtmax (elapsed time #t)))]
-    (let [(update (timestep lander state2 gear dt))]
+    (let [(update (timestep lander state2 dt))]
       (set! lander (car   update))
+      (format #t "~a~&" (position (car (gears lander))))
       (set! state2 (cadr  update)))
     (post-redisplay)))
 
