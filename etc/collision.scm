549b5e76d8c6b26be3e587f54f204d88b7d95765
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-08-30 21:59:47 +0100

    Rotate object around

--- etc/collision.scm
+++ etc/collision.scm
@@ -10,6 +10,10 @@
 (define h2 (/ h 2))
 (define d2 (/ d 2))
 
+(define alpha 0.0)
+(define beta 0.0)
+(define gamma 0.0)
+
 (define vertices
   (list (list (- w2) (- h2) (- d2))
         (list (+ w2) (- h2) (- d2))
@@ -23,6 +27,25 @@
 (define edges '((0 1) (2 3) (4 5) (6 7) (0 2) (1 3) (4 6) (5 7) (0 4) (1 5) (2 6) (3 7)))
 
 (define faces '((0 2 3 1) (4 5 7 6) (0 1 5 4) (2 6 7 3) (0 4 6 2) (1 3 7 5)))
+
+(define (rotate-z gamma)
+  (list (list (cos gamma) (- (sin gamma)) 0)
+        (list (sin gamma) (cos gamma) 0)
+        (list 0 0 1)))
+
+(define (rotate-y beta)
+  (list (list (cos beta) 0 (- (sin beta)))
+        (list 0 1 0)
+        (list (sin beta) 0 (cos beta))))
+
+(define (rotate-x alpha)
+  (list (list 1 0 0)
+        (list 0 (cos alpha) (- (sin alpha)))
+        (list 0 (sin alpha) (cos alpha))))
+
+(define (dot matrix point) (map (lambda (row) (reduce + 0 (map (lambda (m p) (* m p)) row point))) matrix))
+
+(define (rotate matrix vertices) (map (lambda (point) (dot matrix point)) vertices))
 
 (define (index-of a b) (list-index (cut eqv? a <>) b))
 (define (face-edge face edge)
@@ -70,18 +93,26 @@
     (gl-ortho (- w) w (- h) h -100 +100)))
 
 (define (on-display)
-  (gl-clear (clear-buffer-mask color-buffer))
-  (gl-begin (begin-mode lines)
-    (gl-color 1 0 0)
-    (for-each
-      (lambda (edge)
-        (apply gl-vertex (list-ref vertices (car edge)))
-        (apply gl-vertex (list-ref vertices (cadr edge))))
-      edges))
-  (swap-buffers))
+  (let [(rotated (rotate (rotate-z gamma) (rotate (rotate-y beta) (rotate (rotate-x alpha) vertices))))]
+    (gl-clear (clear-buffer-mask color-buffer))
+    (gl-begin (begin-mode lines)
+      (gl-color 1 0 0)
+      (for-each
+        (lambda (edge)
+          (apply gl-vertex (list-ref rotated (car edge)))
+          (apply gl-vertex (list-ref rotated (cadr edge))))
+        edges))
+    (swap-buffers)))
+
+(define (on-idle)
+  (set! alpha (+ alpha 0.05))
+  (set! beta (+ beta 0.02))
+  (set! gamma (+ gamma 0.01))
+  (post-redisplay))
 
 (initialize-glut (program-arguments) #:window-size '(640 . 480) #:display-mode (display-mode rgb double))
 (set! main-window (make-window "sfsim"))
 (set-display-callback on-display)
 (set-reshape-callback on-reshape)
+(set-idle-callback on-idle)
 (glut-main-loop)
