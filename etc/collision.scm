f7a45b612ed7dff361c039906168833af8d3c0b6
Jan Wedekind (Dr) <jan@wedesoft.de>
2019-09-01 22:38:27 +0100

    Change angle increments in test program

--- etc/collision.scm
+++ etc/collision.scm
@@ -175,9 +175,9 @@
     (swap-buffers)))
 
 (define (on-idle)
-  (set! alpha (+ alpha 0.02))
+  (set! alpha (+ alpha 0.021))
   (set! beta (+ beta 0.01))
-  (set! gamma (+ gamma 0.005))
+  (set! gamma (+ gamma 0.0052))
   (post-redisplay))
 
 (initialize-glut (program-arguments) #:window-size '(640 . 480) #:display-mode (display-mode rgb double))
